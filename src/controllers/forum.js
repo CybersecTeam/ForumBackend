a3396fefc0d06f136c1067e7800ba482d80aa4c6
Camilo Alejandro Sanchez Cruz <caasanchezcr@unal.edu.co>
2020-06-29 20:14:29 -0500

    Forum creation

--- src/controllers/forum.js
+++ src/controllers/forum.js
@@ -1,9 +1,34 @@
-const toTest = (req,res) => {
-    res.json({
-        message: "Funciona :D"
+const Forum = req('../models/forum');
+
+
+const createForum = (req,res) => {
+    const body = req.body;
+    const creator = body.nickname;
+    const title = body.title;
+    const forumBody = body.forumBody;
+    const dateCreated = Date.now();
+
+    const newForum = new Forum({
+        title,
+        body: forumBody,
+        comments: [],
+        creator,
+        dateCreated
+    });
+    
+    newForum.save((err) => {
+        if(err){
+            return res.status(500).json({
+                errors: ["Server error"]
+            })
+        }
+
+        res.json({
+            message: "Forum created"
+        })
     })
 }
 
 module.exports = {
-    toTest 
+    createForum 
 }
